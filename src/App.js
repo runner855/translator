cdb3cc6d7c980197a47a46b960a324040c06dcfd
Dario Milani <dmilani855@gmail.com>
2023-02-03 17:29:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
+
+const App = () => {
+  const [language, setLanguage] = useState("es");
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <Field onChange={setText} />
+      <Languages language={language} />
+      <hr />
+      <Translate text={text} language={language} />
+    </div>
+  );
+};
+export default App;
